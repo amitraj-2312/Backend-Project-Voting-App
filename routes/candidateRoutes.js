f05d31a69ba1045cf64b782566e622d687bd4503
amit <amitjhastm2001@gmail.com>
2026-01-16 10:00:01 +0530

    update

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -84,6 +84,27 @@ router.delete('/:candidateID', jwtAuthMiddleware, async (req, res)=>{
     }
 })
 
+// vote count 
+router.get('/vote/count', async (req, res) => {
+    try{
+        // Find all candidates and sort them by voteCount in descending order
+        const candidate = await Candidate.find().sort({voteCount: 'desc'});
+
+        // Map the candidates to only return their name and voteCount
+        const voteRecord = candidate.map((data)=>{
+            return {
+                party: data.party,
+                count: data.voteCount
+            }
+        });
+
+        return res.status(200).json(voteRecord);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
 // let's start voting
 router.get('/vote/:candidateID', jwtAuthMiddleware, async (req, res)=>{
     // no admin can vote
@@ -126,26 +147,7 @@ router.get('/vote/:candidateID', jwtAuthMiddleware, async (req, res)=>{
     }
 });
 
-// vote count 
-router.get('/vote/count', async (req, res) => {
-    try{
-        // Find all candidates and sort them by voteCount in descending order
-        const candidate = await Candidate.find().sort({voteCount: 'desc'});
 
-        // Map the candidates to only return their name and voteCount
-        const voteRecord = candidate.map((data)=>{
-            return {
-                party: data.party,
-                count: data.voteCount
-            }
-        });
-
-        return res.status(200).json(voteRecord);
-    }catch(err){
-        console.log(err);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
-});
 
 // Get List of all candidates with only name and party fields
 router.get('/', async (req, res) => {
